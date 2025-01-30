962f4462eb42147edaa6f67049edfd33365efb91
aryan-patel-1 <aryanpatel150304@gmail.com>
2025-01-30 15:09:23 +0100

    AJOUT DU HEADER

---
+++ src/components/header/header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "../../images/logo/LOGO.svg";
+import "./header.css";
+
+
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="Kasa Logo" className="logo" />
+      <nav>
+        <a href="/">Accueil</a>
+        <a href="/about">À propos</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/page.accueil.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/header/header";  // Importation du composant Header
+
+
+const PageAccueil = () => {
+  return (
+    <div>
+      <Header />  {/* Utilisation du composant Header */}
+
+    </div>
+  );
+};
+
+export default PageAccueil;
